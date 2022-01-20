acedad17b31d34667548e7f0172a06a35c3bd1df
Mark <markgabon@hotmail.com>
2022-01-20 16:35:39 -0500

    Finished asks

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+//Contexts
+import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -13,20 +16,27 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item])
 	};
 
 	return (
 		<div className="App">
+		<ProductContext.Provider value={{products, addItem}}>
+		<CartContext.Provider value={cart}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
+
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products />
 			</Route>
 
+
 			<Route path="/cart">
-				<ShoppingCart cart={cart} />
+				<ShoppingCart />
 			</Route>
+			</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
